e33684bb7cbb90729f93bcbec50add52f9028abd
baxter <baxter.clements@mymail.champlain.edu>
2019-02-23 20:22:59 -0500

    script

---
+++ secure-ssh.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+useradd sys265
+mkdir /home/sys265/.ssh
+mkdir /home/sys265/.ssh/authorized_keys
+cp linux/public-keys/sys265.pub /home/sys265/.ssh/authorized_keys
+chmod 700 /home/sys265/.ssh
+chmod 600 /home/sys265/.ssh/authorized_keys
+chown -R sys265:sys265 /home/sys265/.ssh
